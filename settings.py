826048608b05687be6f611cbf808ea77d2d295fc
George Paradox <paradox81ru@yandex.ru>
2022-04-13 11:58:40 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,17 @@
+TEMPLATES = [
+    {
+        'NAME': 'jinja2',
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'environment': 'DjangoProject.jinja2.environment',
+            'autoescape': False,
+            'trim_blocks': True,
+            "extensions": [
+                "jinja2.ext.do",
+                "jinja2.ext.i18n",
+                "path_to_extensions.TemplateCacheExtension"
+            ],
+        }
+    }
+]
